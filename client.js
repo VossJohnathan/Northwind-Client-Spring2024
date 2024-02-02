be736064b2dd2561cc8f40d3235847190637d16b
Johnathan Voss <JVoss11@my.wctc.edu>
2024-02-01 19:12:22 -0600

    Week 2 Assignment Done

--- client.js
+++ client.js
@@ -1,7 +1,18 @@
 $(function(){
+    $(document).on('keydown', function(e) {
+        if (e.keyCode == 27) {
+            $('#liveToast').toast('hide')
+        }
+    });
     // preload audio
     var toast = new Audio('toast.wav');
     $('.code').on('click', function(e) {
+        //display selected product info
+        const name = $(this).data('product');
+        const code = $(this).data('code'); //code in place of product to show discount code.
+        $('#name').html(name);
+        $('#code').html(code);
+
         e.preventDefault();
         // first pause the audio (in case it is still playing)
         toast.pause();
